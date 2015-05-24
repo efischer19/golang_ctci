aca60e080286baa7f1b888fba9ba63a3f417aa32
efischer0119 <efischer0119@gmail.com>
2015-05-24 12:04:47 -0400

    giving 2.go a package so test compilation doesn't complain

--- ch_1/2.go
+++ ch_1/2.go
@@ -1,1 +1,3 @@
 //1.2 omitted, as it explicitly asks for C/C++, and I already more-or-less solved this when setting up go via the example at https://golang.org/doc/code.html
+
+package ctci_chapter1
