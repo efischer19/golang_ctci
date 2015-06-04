3adb7677c3bcd69d118be1e4faf04c8d8e69b642
efischer0119 <efischer0119@gmail.com>
2015-06-03 20:36:38 -0400

    Fixing bug in RemoveDupesNaive uncovered by the for loop issues fix in ListEquals. Was not removing the final element of a list when it needed to be removed, logic just wan't right on the edge case here

--- ch_2/1_RemoveDupes.go
+++ ch_2/1_RemoveDupes.go
@@ -3,9 +3,11 @@ package ctci_chapter2
 //this approach uses no extra space, but runs in O(n^2) time
 func (n *Node) RemoveDupesNaive() {
 	for cur := n; cur != nil; cur = cur.next {
-		for inner := cur; inner != nil && inner.next != nil; inner = inner.next {
+		for inner := cur; inner.next != nil;{
 			if cur.data == inner.next.data {
 				inner.next = inner.next.next
+			} else {
+				inner = inner.next
 			}
 		}
 	}
